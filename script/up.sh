76be5ac9b9c0e86181d5c9f83fdbf49ed2e9e83f
Hilary Liu <2788370451@qq.com>
2023-07-01 21:16:20 +0800

    拓展shell脚本

--- script/up.sh
+++ script/up.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
-npm run build
 
-npm run up
+# 扩展一下命令行功能
+# 仅判断第一个参数 1. b（只打包）; 2. bu or ub（先打包然后创建）;3. 不传就是（先打包然后创建）
+
+OPTION=$1
+
+case $OPTION in
+"b")
+    echo "开始进行打包";
+    npm run build;
+;;
+"bu")
+    echo "开始进行打包";
+    npm run build;
+    echo "开始进行上传";
+    npm run up;
+;;
+"ub")
+    echo "开始进行打包";
+    npm run build;
+    echo "开始进行上传";
+    npm run up;
+;;
+"")
+    echo "开始进行打包";
+    npm run build;
+    echo "开始进行上传";
+    npm run up;
+;;
+*)
+  echo "参数错误！"
+esac
+
